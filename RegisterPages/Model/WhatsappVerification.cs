0db7b9f1594e4cea4d1718b15a71015d86b1e138
onny <onny.s@students.amikom.ac.id>
2021-01-09 14:37:59 +0700

    Add logic

--- RegisterPages/Model/WhatsappVerification.cs
+++ RegisterPages/Model/WhatsappVerification.cs
@@ -6,5 +6,12 @@ namespace RegisterPages.Model
 {
     class WhatsappVerification
     {
+        public event EventHandler waVerificationEvent;
+        public void OnUserRegistered(object source, EventArgs e)
+        {
+            //any process and logic here...
+            waVerificationEvent(this, EventArgs.Empty);
+
+        }
     }
 }
