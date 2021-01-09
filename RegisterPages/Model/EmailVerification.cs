41e328e22e1f56b1141d4a1aad5abfa05fcb44e5
onny <onny.s@students.amikom.ac.id>
2021-01-09 14:38:31 +0700

    Add Logic EventHandler

--- RegisterPages/Model/EmailVerification.cs
+++ RegisterPages/Model/EmailVerification.cs
@@ -6,5 +6,12 @@ namespace RegisterPages.Model
 {
     class EmailVerification
     {
+        public event EventHandler emailVerificationEvent;
+        public void OnUserRegistered(object source, EventArgs e)
+        {
+            //any process and logic here...
+            emailVerificationEvent(this, EventArgs.Empty);
+
+        }
     }
 }
